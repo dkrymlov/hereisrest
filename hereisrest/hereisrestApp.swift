363880925b40cf6b2faa42bee9f9c8449c26eb0d
Danylo Krymlov <danil.krymlov@gmail.com>
2024-02-04 14:04:37 +0200

    Initial Commit

---
+++ hereisrest/hereisrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hereisrestApp.swift
+//  hereisrest
+//
+//  Created by Даниил Крымлов on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct hereisrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
